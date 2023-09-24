30369b77edc08ee17ba7662b77bcfc2cac5767d6
fishyboi@MSI <fishyboi@MSI>
2023-09-24 17:58:22 -0400

    Display Data

--- src/index.js
+++ src/index.js
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import ReactDOM from 'react-dom/client';
+import axios from 'axios';
 
 const App = ()=> {
+  const [movies, setMovies] = useState([])
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const {data} = await axios('/api/movies');
+      setMovies(data);
+    }
+    fetchData();
+  }, [])
+  
   return (
-    <h1>FullStack Template</h1>
+    <div>
+      <h1>Some Movies</h1>
+      <ul>
+        {
+          movies.map((movie) => {
+            return(
+              <li key={movie.id}>
+                <h2>{movie.title}</h2>
+                <h3>Rating: {movie.stars} Stars</h3>
+              </li>
+            )
+          })
+        }
+      </ul>
+    </div>
   );
 };
 
