1c3ef5f2bbfc208ce50ac3e06e8bcf640a406139
fishyboi@MSI <fishyboi@MSI>
2023-09-24 23:28:14 -0400

    +- Ratings

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import axios from 'axios';
 
 const App = ()=> {
   const [movies, setMovies] = useState([])
+  const [error, setError] = useState('')
 
   useEffect(() => {
     const fetchData = async () => {
@@ -12,17 +13,68 @@ const App = ()=> {
     }
     fetchData();
   }, [])
+
+  const increaseRating = async(movie) => {
+    try {
+      setError("")
+      const newRating = movie.stars + 1
+      const {data} = await axios.put(`/api/movies/${movie.id}`, {title: movie.title, stars: newRating})
+      console.log(data)
+      const newMovies = movies.map((movieMap) => {
+        if(movieMap.id === movie.id){
+          return data
+        }else{
+          return movieMap
+        }
+      })
+      setMovies(newMovies)
+    }catch (error) {
+      console.log(error.response.data)
+      setError(error.response.data)
+    }
+  }
+
+  const decreaseRating = async(movie) => {
+    try {
+      setError("")
+      const newRating = movie.stars - 1
+      const {data} = await axios.put(`/api/movies/${movie.id}`, {title: movie.title, stars: newRating})
+      console.log(data)
+      const newMovies = movies.map((movieMap) => {
+        if(movieMap.id === movie.id){
+          return data
+        }else{
+          return movieMap
+        }
+      })
+      setMovies(newMovies)
+    }catch (error) {
+      console.log(error.response.data)
+      setError(error.response.data)
+    }
+  }
   
   return (
     <div>
       <h1>Some Movies</h1>
+      <p>{error ? error: ""}</p>
       <ul>
         {
           movies.map((movie) => {
             return(
               <li key={movie.id}>
                 <h2>{movie.title}</h2>
-                <h3>Rating: {movie.stars} Stars</h3>
+                <h3>
+                  <span>
+                    Rating: {movie.stars} Stars
+                    <button onClick={() => {increaseRating(movie)}}>
+                      +
+                    </button>
+                    <button onClick={() => {decreaseRating(movie)}}>
+                      -
+                    </button>
+                  </span>
+                </h3>
               </li>
             )
           })
